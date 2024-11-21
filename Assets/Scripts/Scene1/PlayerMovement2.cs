930a7bfefbd99152e1c32c9908cdc8973ce2611c
Reza Taheri <taheri.reza.01@gmail.com>
2024-11-21 17:24:52 +0330

    adding air move

--- Assets/Scripts/Scene1/PlayerMovement2.cs
+++ Assets/Scripts/Scene1/PlayerMovement2.cs
@@ -2,41 +2,40 @@ using UnityEngine;
 
 public class PlayerMovement2 : MonoBehaviour
 {
-    [SerializeField] private float speed;
+[SerializeField] private float speed; // Horizontal movement speed
+    [SerializeField] private float jumpForce; // Vertical jump force
+    [SerializeField] private float airControlFactor = 0.5f; // Control factor for air movement
 
     public Rigidbody2D body;
     private Animator animator;
     private bool grounded = true;
     private bool run = false;
-
     private Quaternion lockedRotation;
 
     private void Awake()
     {
-        // Grab references from rigidbody and animator from object
+        // Grab references from Rigidbody and Animator
         lockedRotation = transform.rotation;
-        // body = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>(); // change run value
     }
 
     private void Update()
     {
-        transform.rotation = lockedRotation;
-        // float horizontalInput = Input.GetAxis("Horizontal");
-        // body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        transform.rotation = lockedRotation; // Lock rotation to prevent flipping
+
+        // Horizontal movement
+        float horizontalInput = 0;
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += Vector3.right * speed * Time.deltaTime;
-            // flip player
-            transform.localScale = Vector3.one;
+            horizontalInput = 1; // Move right
+            transform.localScale = Vector3.one;; // Flip sprite to face right
             run = true;
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            transform.position += Vector3.right * -speed * Time.deltaTime;
-            // flip player
-            transform.localScale = new Vector3(-1, 1, 1);
+            horizontalInput = -1; // Move left
+            transform.localScale = new Vector3(-1, 1, 1); // Flip sprite to face left
             run = true;
         }
         else
@@ -44,33 +43,41 @@ public class PlayerMovement2 : MonoBehaviour
             run = false;
         }
 
-        if (Input.GetKey(KeyCode.W) && grounded)
+        // Apply movement based on grounded state
+        if (grounded)
+        {
+            // Full control on the ground
+            body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        }
+        else
+        {
+            // Limited air control
+            body.velocity = new Vector2(horizontalInput * speed * airControlFactor, body.velocity.y);
+        }
+
+        // Jumping
+        if (Input.GetKeyDown(KeyCode.W) && grounded)
+        {
             Jump();
+        }
 
-
-        // Set animator parameter
+        // Update animator parameters
         animator.SetBool("Run", run);
-        // update animator grounded
         animator.SetBool("Grounded", grounded);
     }
 
     private void Jump()
     {
-        body.velocity = new Vector2(body.velocity.x, speed);
+        body.velocity = new Vector2(body.velocity.x, jumpForce); // Apply upward force for jump
         animator.SetTrigger("Jump");
-        grounded = false;
+        grounded = false; // Player is no longer grounded
     }
 
-    /// <summary>
-    /// Sent when an incoming collider makes contact with this object's
-    /// collider (2D physics only).
-    /// </summary>
-    /// <param name="other">The Collision2D data associated with this collision.</param>
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
             grounded = true;
-        else if (collision.gameObject.tag == "Ball")
-            grounded = true;
+        // else if (collision.gameObject.tag == "Ball")
+        //     grounded = true;
     }
 }
