15a94c46cd7a5c6116bf428da24d7d55105b0b0f
Reza Taheri <taheri.reza.01@gmail.com>
2024-11-28 18:50:46 +0330

    Update FPSLimiter.cs
    remove fps display

--- Assets/Scripts/MenuScene/FPSLimiter.cs
+++ Assets/Scripts/MenuScene/FPSLimiter.cs
@@ -6,19 +6,11 @@ using TMPro;
 public class FPSLimiter : MonoBehaviour
 {
     public int targetFPS = 30;
-    public TextMeshProUGUI FPSText;
-    private float currentFPS;
     // Start is called before the first frame update
     void Start()
     {
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = targetFPS;
-        InvokeRepeating("GetFPS", 1, 1);
-    }
-
-    void GetFPS(){
-        currentFPS = (int) (1f / Time.unscaledDeltaTime);
-        FPSText.text = currentFPS.ToString();
     }
 }
 
