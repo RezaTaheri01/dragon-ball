d1ed54faea97498b7bd8cf794c9f6502dd7abab9
Reza Taheri <taheri.reza.01@gmail.com>
2024-12-02 02:01:58 +0330

    Update GameManager.cs

--- Assets/Scripts/Scene1/GameManager.cs
+++ Assets/Scripts/Scene1/GameManager.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro; // Required for TextMesh Pro
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Image image;
     public TextMeshProUGUI Mode;
     public GameObject player;  // The player GameObject
     public GameObject mainCam;  // The player GameObject
@@ -33,11 +35,13 @@ public class GameManager : MonoBehaviour
         {
             SwitchToPlayerControl();
             Mode.text = "Two Player";
+            EnableImage();
         }
         else
         {
             SwitchToAIControl();
             Mode.text = "Single Player";
+            DisableImage();
 
         }
         Init.reset();
@@ -58,5 +62,17 @@ public class GameManager : MonoBehaviour
         aiPlayerScript.enabled = false;  // Disable AI control
         playerMovementScript.enabled = true;  // Enable player control
         isAIControlled = false;  // Set control flag to player
+    }
+
+    public void EnableImage()
+    {
+        if (image != null)
+            image.enabled = true; // Enables the image
+    }
+
+    public void DisableImage()
+    {
+        if (image != null)
+            image.enabled = false; // Disables the image
     }
 }
